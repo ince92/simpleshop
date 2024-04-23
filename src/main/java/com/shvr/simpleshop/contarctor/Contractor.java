3e084dc8f5366c367d729eb09d1b1dc21670ac98
ince92 <ezar4992@uandex.ru>
2024-04-23 14:43:00 +0400

    add schema.sql

--- src/main/java/com/shvr/simpleshop/contarctor/Contractor.java
+++ src/main/java/com/shvr/simpleshop/contarctor/Contractor.java
@@ -1,5 +1,8 @@
 package com.shvr.simpleshop.contarctor;
 
+import lombok.Getter;
+
+@Getter
 public class Contractor {
     private long id;
     private String name;
--- src/main/java/com/shvr/simpleshop/order/Order.java
+++ src/main/java/com/shvr/simpleshop/order/Order.java
@@ -1,10 +1,12 @@
 package com.shvr.simpleshop.order;
 
 import com.shvr.simpleshop.orderLine.OrderLine;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
 public class Order {
     private LocalDateTime date;
     private long Id;
