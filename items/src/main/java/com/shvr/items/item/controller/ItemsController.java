148f40041018e621b79d831b7ad53aa5432d686b
ince92 <ezar4992@uandex.ru>
2024-04-25 10:43:55 +0400

    add test controller

---
+++ items/src/main/java/com/shvr/items/item/controller/ItemsController.java
@@ -0,0 +1,16 @@
+package com.shvr.items.item.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/items")
+public class ItemsController {
+
+    @GetMapping
+    public String helloitem(){
+        return "hello";
+    }
+
+}
