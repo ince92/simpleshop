e48071cac0ed07cbd072b0284e048fe49d67c015
ince92 <ezar4992@uandex.ru>
2024-05-30 23:29:25 +0400

    add data annotation

--- orders/src/main/java/com.shvr.orders/order/Order.java
+++ orders/src/main/java/com.shvr.orders/order/Order.java
@@ -1,11 +1,12 @@
 package com.shvr.orders.order;
 
 import com.shvr.orders.orderLine.OrderLine;
+import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Data
 @Getter
 public class Order {
     private LocalDateTime date;
